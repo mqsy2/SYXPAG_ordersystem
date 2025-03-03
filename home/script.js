b5ead959745017c3a2a2b19304721f0da36b4781
JcAPC <jaycemarkpresbitero1902@gmail.com>
2025-03-03 23:11:28 +0800

    update

--- home/script.js
+++ home/script.js
@@ -1,23 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addRowBtn = document.getElementById("add-row-btn");
-    const orderList = document.getElementById("order-list");
+document.getElementById('add-row-btn').addEventListener('click', function() {
+    const orderList = document.getElementById('order-list');
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td><input type="number" class="form-control" value="1" min="1"></td>
+        <td><input type="text" class="form-control" placeholder="Enter product name"></td>
+        <td><input type="number" class="form-control" placeholder="Enter price"></td>
+        <td><button class="btn btn-danger remove-btn">✖</button></td>
+    `;
+    orderList.appendChild(newRow);
 
-    if (addRowBtn) {
-        addRowBtn.addEventListener("click", function() {
-            const newRow = document.createElement("tr");
-
-            newRow.innerHTML = `
-                <td><input type="number" class="form-control" value="1" min="1"></td>
-                <td><input type="text" class="form-control" placeholder="Enter product name"></td>
-                <td><input type="number" class="form-control" placeholder="Enter price"></td>
-                <td><button class="btn btn-danger remove-btn">✖</button></td>
-            `;
-
-            orderList.appendChild(newRow);
-
-            newRow.querySelector(".remove-btn").addEventListener("click", function() {
-                newRow.remove();
-            });
-        });
-    }
+    newRow.querySelector('.remove-btn').addEventListener('click', function() {
+        newRow.remove();
+    });
 });
